90393a5d8e7483ff4d7a282cc3a25fa2feb9edb9
Borschik <47531847+Borschik1@users.noreply.github.com>
2022-06-03 17:10:03 +0500

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,47 @@
+#include <vector>
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <map>
+#include <queue>
+#include <cstdlib>
+#include <iomanip>
+#include <algorithm>
+#include <cmath>
+#include <cassert>
+
+using namespace std;
+
+
+int main() {
+    ifstream fin("D:/Desktop/INPUT.txt");
+    ofstream fout("D:/Desktop/OUTPUT.txt");
+    
+    int h, w, n;
+    fin >> h >> w >> n;
+    vector <vector <int>> sp;
+    for (int i = 0; i < h; i++) {
+        vector <int> prov(w);
+        for (int j = 0; j < w; j++) {
+            fin >> prov[j];
+        }
+        sp.push_back(prov);
+    }
+    for (int i = 0; i < n; i++) {
+        int x1, y1, x2, y2;
+        int answer = 0;
+        fin >> x1 >> y1 >> x2 >> y2;
+        for (int j = x1; j <= x2; j++) {
+            for (int k = y1; k <= y2; k++) {
+                answer += sp[j - 1][k - 1];
+            }
+        }
+        fout << answer << endl;
+    }
+
+    fin.close();
+    fout.close();
+    std::system("pause");
+    return 0;
+}
+
